307ed083ec756510ccae202151895035e797c3ff
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-19 17:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let min_cost=0;
+	arr.sort(function(a,b) {return a-b});
+	
+     for(let i=1;i<arr.length;i++)
+		 {
+			 min_cost=min_cost+arr[i]+arr[i-1];
+			 arr[i]=arr[i]+arr[i-1];
+		 }
+	return min_cost;
 }
 
 module.exports=mincost;
